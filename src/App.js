43afb2b9c9d3f710083a5e6caea90bc8157e4b04
Raymond Cheung <korekontakt@gmail.com>
2024-08-26 10:50:19 +0100

    Finish implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -10,7 +10,6 @@ const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
-
 
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
@@ -21,6 +20,9 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockDetailsAvailable, setBlockNumberDetailsAvailable] =
+    useState(false);
+  const [transactionDetails, setTransactionDetails] = useState([]);
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -30,7 +32,38 @@ function App() {
     getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  const fetchTransactionDetails = async () => {
+    const block = await alchemy.core.getBlock();
+    setBlockNumberDetailsAvailable(true);
+
+    const txDetails = await Promise.all(
+      block.transactions.map((tx) => alchemy.core.getTransactionReceipt(tx))
+    );
+    setTransactionDetails(txDetails);
+  };
+
+  return (
+    <div>
+      <div className="App">Block Number: {blockNumber}</div>
+      <div className="App">
+        <button onClick={fetchTransactionDetails}>Get Block Details</button>
+
+        {blockDetailsAvailable &&
+          transactionDetails.map((txDetails, i) => (
+            <div key={i}>
+              <h2>Transaction</h2>
+              <ul>
+                <li>Transaction Hash: {txDetails.transactionHash}</li>
+                <li>Transaction Index: {txDetails.transactionIndex}</li>
+                <li>From: {txDetails.from}</li>
+                <li>To: {txDetails.to}</li>
+              </ul>
+              <hr />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
